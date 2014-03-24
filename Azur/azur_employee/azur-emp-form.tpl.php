e212627eb49d864f3f1457c0f1234c61a568ce9c
rarvind1991 <rarvind@azurinfotech.com>
2014-03-25 03:21:56 +0530

    Changes and bug fixes

--- Azur/azur_employee/azur-emp-form.tpl.php
+++ Azur/azur_employee/azur-emp-form.tpl.php
@@ -1,8 +1,8 @@
 <table class="form-table">
   <tbody>
     <tr><td ><?php print drupal_render($form['firstname']); ?></td><td ><?php print drupal_render($form['lastname']); ?></td></tr>
-  	<tr><td><?php print drupal_render($form['enum']); ?></td><td ><?php print drupal_render($form['type']); ?></td></tr>
-    <tr><td><?php print drupal_render($form['sdate']); ?></td><td ><?php print drupal_render($form['qualification']); ?></td></tr>
+  	<tr><td><?php print drupal_render($form['qualification']); ?></td><td ><?php print drupal_render($form['type']); ?></td></tr>
+    <tr><td><?php print drupal_render($form['sdate']); ?></td></tr>
     </tbody>
 </table>
 <?php print drupal_render_children($form); ?>
