a3883c8be3cacd0e75b127c0a525271a726d061c
rarvind1991 <rarvind@azurinfotech.com>
2014-03-27 16:12:18 +0530

    Changes to bid management

---
+++ Azur/bid_management/bid-management-form.tpl.php
@@ -0,0 +1,6 @@
+<table class="form-table" style="border:1px solid; padding:20px; box-shadow:-5px 5px 5px 5px #CCC">
+  <tbody>
+    <tr style="margin-left:20px;margin-right: 20px;"><td width="20%" style="vertical-align: top; padding: 20px;"><?php print drupal_render($form['help_text']); ?></td><td style="padding: 20px;"><?php print drupal_render_children($form); ?></td></tr>
+    </tbody>
+</table>
+<?php print drupal_render_children($form); ?>
