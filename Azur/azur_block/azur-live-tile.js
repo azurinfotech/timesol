0dd9d9beb13720f8d3a2b97b1c5eb6105dbb7280
rarvind1991 <rarvind@azurinfotech.com>
2014-04-04 23:53:36 +0530

    final push version 1.0

---
+++ Azur/azur_block/azur-live-tile.js
@@ -0,0 +1,15 @@
+(function($) {
+    $(document).ready(function() {
+    $('.live-azur').each(function(){
+        var x =$('this');
+        if(this.classList[2] == "active"){
+            $('this').hide();
+            this.removeClass('active');
+            if()
+        }
+        else{
+            $('this').hide();
+        }
+    });
+    });
+})(jQuery);
--- Azur/azur_employee/azur-emp-form.tpl.php
+++ Azur/azur_employee/azur-emp-form.tpl.php
@@ -1,8 +1,13 @@
 <table class="form-table">
-  <tbody>
-    <tr><td ><?php print drupal_render($form['firstname']); ?></td><td ><?php print drupal_render($form['lastname']); ?></td></tr>
-  	<tr><td><?php print drupal_render($form['qualification']); ?></td><td ><?php print drupal_render($form['type']); ?></td></tr>
-    <tr><td><?php print drupal_render($form['sdate']); ?></td></tr>
+    <tbody>
+    <tr><td><?php print drupal_render($form['firstname']);?></td>
+        <td><?php print drupal_render($form['lastname']);?></td></tr>
+    <tr><td><?php print drupal_render($form['qualification']);?></td>
+        <td><?php print drupal_render($form['departments']);?></td></tr>
+    <tr><td><?php print drupal_render($form['type']);?></td>
+        <td><?php print drupal_render($form['payment']);?></td></tr>
+    <tr><td><?php print drupal_render($form['mgr_id']);?></td>
+        <td><?php print drupal_render($form['sdate']);?></td></tr>
     </tbody>
 </table>
 <?php print drupal_render_children($form); ?>
